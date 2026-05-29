e72eec0dd92c78735caaf8374a213058bd643580
tristanbollard <tristan.bollard@gmail.com>
2026-05-29 16:45:26 +1000

    fix binding

--- system_files/usr/lib/hyprbazzite/etc/hypr/bindings.lua
+++ system_files/usr/lib/hyprbazzite/etc/hypr/bindings.lua
@@ -92,4 +92,4 @@ hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
 -- LID SWITCH
 hl.bind("switch:on:Lid Switch", hl.dsp.exec_cmd("/etc/hypr/scripts/lidact.sh off"), { locked = true })
 hl.bind("switch:off:Lid Switch", hl.dsp.exec_cmd("/etc/hypr/scripts/lidact.sh on"), { locked = true })
-hl.bind("SUPER_SHIFT, D", hl.dsp.exec_cmd("/etc/hypr/scripts/lidact.sh toggle"), { locked = true })
+hl.bind(mainMod .. " + SHIFT + D", hl.dsp.exec_cmd("/etc/hypr/scripts/lidact.sh toggle"), { locked = true })
